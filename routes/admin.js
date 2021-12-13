b53c487cf507702b373b2c3a4c9d69ff4c909af0
diego <diegopgcds10@gmail.com>
2021-12-13 17:31:51 -0300

    Administração do Menu

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const { route } = require(".");
 var users = require("./../inc/users");
+var admin = require("./../inc/admin");
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -17,6 +18,14 @@ router.use(function(req, res, next){
 
 });
 
+router.use(function(req, res, next){
+
+    req.menus = admin.getMenus();
+
+    next();
+
+});
+
 router.get("/logout", function(req, res, next){
 
     delete req.session.user;
@@ -27,7 +36,11 @@ router.get("/logout", function(req, res, next){
 
 router.get("/", function(req, res, next){
 
-        res.render("admin/index");
+        res.render("admin/index", {
+
+            menus: req
+
+        });
 
 });
 
@@ -63,19 +76,31 @@ router.post("/login", function(req, res, next){
 
 router.get("/contacts", function(req, res, next){
 
-    res.render("admin/contacts");
+    res.render("admin/contacts", {
+
+        menus: req
+
+    });
 
 });
 
 router.get("/emails", function(req, res, next){
 
-    res.render("admin/emails");
+    res.render("admin/emails", {
+
+        menus: req
+
+    });
 
 });
 
 router.get("/menus", function(req, res, next){
 
-    res.render("admin/menus");
+    res.render("admin/menus", {
+
+        menus: req
+
+    });
 
 });
 
@@ -83,6 +108,7 @@ router.get("/reservations", function(req, res, next){
 
     res.render("admin/reservations", {
 
+        menus: req,
         date:{}
 
     });
@@ -91,7 +117,11 @@ router.get("/reservations", function(req, res, next){
 
 router.get("/users", function(req, res, next){
 
-    res.render("admin/users");
+    res.render("admin/users", {
+
+        menus: req
+
+    });
 
 });
 
