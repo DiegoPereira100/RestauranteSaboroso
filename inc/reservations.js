0619f045dc29a4ca1b4e5bbb38ab448fdb76f105
diego <diegopgcds10@gmail.com>
2021-12-21 11:32:03 -0300

    Trabalhando em Tempo Real com Socket.io, emit e On

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,5 +1,6 @@
 var conn = require('./db');
 var Pagination = require("./../inc/Pagination");
+var moment = require("moment");
 
 module.exports = {
 
@@ -137,5 +138,51 @@ module.exports = {
 
         });
 
+    },
+
+    chart(req){
+
+        return new Promise((resolve, reject)=> {
+
+            conn.query(`SELECT 
+                            CONCAT(YEAR(date), '-', MONTH(date)) AS date,
+                            COUNT(*) AS total,
+                            SUM(people) / COUNT(*) AS avg_people
+                            FROM tb_reservations
+                            WHERE
+                                date BETWEEN  ? AND ?
+                                GROUP BY YEAR(date) DESC, MONTH(date) DESC
+                                ORDER BY YEAR(date) DESC, MONTH(date) DESC;
+                            `,[
+                                req.query.start,
+                                req.query.end
+                            ], (err, results)=>{
+
+                                if (err) {
+
+                                    reject(err);
+
+                                }else {
+
+                                    let months = [];
+                                    let values = [];
+
+                                    results.forEach(row=> {
+
+                                        months.push(moment(row.date).format('MMMM YYYY'));
+                                        values.push(row.total);
+
+                                    });
+
+                                    resolve({
+                                        months,
+                                        values
+                                    });
+
+                                }
+                            }
+
+                            );
+        });
     }
 };
